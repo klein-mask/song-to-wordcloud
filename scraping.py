265b57ebf195ed38e9fac729e27a55518fc19896
kasamizu <kasamizu@4cast.co.jp>
2020-01-20 12:42:34 +0900

    scp

--- scraping.py
+++ scraping.py
@@ -4,26 +4,37 @@ import pandas as pd
 import time
 
 
+class LiricsToWordCloud:
+
+    def __init__(self):
+        self.lirics = ''
+        self.artist = ''
+        self.song = ''
+    
+    def get_lirics(self, *, url=''):
+        res = requests.get(url)
+
+        try:
+            soup = BeautifulSoup(res.text, "lxml")
+        except:
+            soup = BeautifulSoup(res.text, "html5lib")
+        
+        self.song = soup.find(class_='prev_pad').text.replace('\n','')
+        self.lirics = soup.find('div', itemprop='lyrics').text.replace('\n','')
+        self.artist = soup.find('span', itemprop='byArtist name').text.replace('\n','')
+
+        print(self.song)
+        print(self.artist)
+
+
+        # create dataframe
+        df = pd.DataFrame(data=[[self.song, self.artist, self.lirics]], columns=['曲名', 'アーティスト名', '歌詞'])
+
+        print(df)
+
+        df.to_csv('lirics.csv', mode = 'a', encoding='utf-8')
+
+
 if __name__ == "__main__":
-    #スクレイピングしたデータを入れる表を作成
-    list_df = pd.DataFrame(columns=['歌詞'])
-
-    #曲ページ先頭アドレス
-    base_url = 'https://www.uta-net.com'
-
-    #歌詞一覧ページ
-    url = 'https://www.uta-net.com/song/266648/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    song_lyrics = soup.find('div', itemprop='lyrics')
-    song_lyric = song_lyrics.text
-    song_lyric = song_lyric.replace('\n','')
-
-    #取得した歌詞を表に追加
-    tmp_se = pd.DataFrame([song_lyric], index=list_df.columns).T
-    list_df = list_df.append(tmp_se)
-
-    print(list_df)
-
-    #csv保存
-    list_df.to_csv('list.csv', mode = 'a', encoding='utf-8')
+    ltw = LiricsToWordCloud()
+    ltw.get_lirics(url='https://www.uta-net.com/song/266648/')
